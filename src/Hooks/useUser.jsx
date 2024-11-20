2e5d47ebc9e7cc50d3df09bd8326c51177bda396
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-20 18:51:33 +0600

    have to do dynamic

--- src/Hooks/useUser.jsx
+++ src/Hooks/useUser.jsx
@@ -3,11 +3,12 @@ import useAxiosCommon from "./useAxiosCommon";
 import { useQuery } from "@tanstack/react-query";
 
 const useUser = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const axiosCommon = useAxiosCommon();
 
   const { data: userData = {} } = useQuery({
-    queryKey: ["userData", user.email],
+    enabled: !loading,
+    queryKey: ["userData", user?.email],
     queryFn: async () => {
       const { data } = await axiosCommon.get(`/user/${user?.email}`);
       return data;
--- src/Page/Dashboard/Sidebar.jsx
+++ src/Page/Dashboard/Sidebar.jsx
@@ -1,8 +1,28 @@
 import { NavLink } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import { TbLogout2 } from "react-icons/tb";
+import useUser from "../../Hooks/useUser";
+import { FaHeart } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
 
 const Sidebar = () => {
+  const userData = useUser();
+
+  const buyerLink = [
+    {
+      id: 1,
+      path: "/wishlist",
+      title: "Wishlist",
+      icon: <FaHeart size={24} />,
+    },
+    {
+      id: 2,
+      path: "/cart",
+      title: "Cart",
+      icon: <FaCartShopping size={24} />,
+    },
+  ];
+
   return (
     <div className="min-h-screen h-full pl-2 pt-8 bg-amber-400">
       <NavLink to="/" className="text-2xl btn font-bold font-serif">
@@ -11,11 +31,14 @@ const Sidebar = () => {
       {/* LINKN ITEMS */}
       <div className="my-6 px-4 ">
         <div>
-          <NavLink
-            className={`btn flex items-center justify-start btn-block my-1`}
-          >
-            <FaHome size={28} /> <span className="text-xl">Home</span>
-          </NavLink>
+          {buyerLink.map((item) => (
+            <NavLink
+              key={item.id}
+              className={`btn flex items-center justify-start btn-block my-2`}
+            >
+              {item.icon} <span className="text-xl">{item.title}</span>
+            </NavLink>
+          ))}
         </div>
         <div className="">
           <button className={`btn flex items-center justify-start btn-block`}>
