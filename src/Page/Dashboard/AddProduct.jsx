9650d76fee56480a0579279f0238ede86649ead5
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-20 20:42:01 +0600

    designing add product

--- src/Page/Dashboard/AddProduct.jsx
+++ src/Page/Dashboard/AddProduct.jsx
@@ -9,16 +9,38 @@ const AddProduct = () => {
       />
       <div>
         <form>
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text">What is your name?</span>
-            </div>
-            <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered w-full max-w-xs"
-            />
-          </label>
+          <div className="grid grid-cols-1 md:grid-cols-2 border-2 px-4 lg:px-12">
+            <label className="form-control w-full max-w-md">
+              <div className="label">
+                <span className="label-text">What is your name?</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-md"
+              />
+            </label>
+            <label className="form-control w-full max-w-md">
+              <div className="label">
+                <span className="label-text">What is your name?</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-md"
+              />
+            </label>
+            <label className="form-control w-full max-w-md">
+              <div className="label">
+                <span className="label-text">What is your name?</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-md"
+              />
+            </label>
+          </div>
         </form>
       </div>
     </div>
