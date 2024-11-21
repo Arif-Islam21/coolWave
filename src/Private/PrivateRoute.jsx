c015f3b0bae608a9a3259133750befd35677234e
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-21 16:56:27 +0600

    simple bug fixed

--- src/Private/PrivateRoute.jsx
+++ src/Private/PrivateRoute.jsx
@@ -1,14 +1,16 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
+import useUser from "../Hooks/useUser";
 
 // eslint-disable-next-line react/prop-types
 const PrivateRoute = ({ children }) => {
   const location = useLocation();
   const { user, loading } = useAuth();
-  if (loading) {
+  const { userData, isLoading } = useUser();
+  if (loading || isLoading) {
     return <p className="text-3xl text-center font-bold">Loading....</p>;
   }
-  if (user) {
+  if (user && userData) {
     return children;
   }
 
